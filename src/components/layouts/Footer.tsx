c0eace8c0f03e6bc6584553f6edef0be83ad3584
mohamed salah <mohamedsalah.software@gmail.com>
2024-05-04 16:27:29 +0300

    bottomBar

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,12 +1,14 @@
 const Footer = () => {
   return (
-    <div className="footer">
-      <div>
-        <i></i>
-        <h1>Foot Content</h1>
-        <p className="text-center">Leave empty</p>
+    <>
+      <div className="footer">
+        <div>
+          <i></i>
+          <h1>Foot Content</h1>
+          <p className="text-center">Leave empty</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
